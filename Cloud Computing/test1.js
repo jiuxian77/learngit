ee3df57d40c5e59ebf94b5ac7381e65e48923bcd
jiuxian77 <zjsxjiuxian77@163.com>
2018-10-23 18:10:05 +0800

    understand how stage works

---
+++ Cloud Computing/test1.js
@@ -0,0 +1,23 @@
+'use strict';
+//console.log('hello world');
+//
+//var s = new Set(['A', 'B', 'C']);
+//s.forEach(function (element, sameElement, set) {
+//  console.log(sameElement);
+//});
+
+// Load the SDK and UUID
+
+var http = require("http");
+
+http.createServer(function(request, response) { 
+ 
+    response.writeHead(200, {"Content-Type": "text/html"}); 
+ 
+    response.write("<html><head><title>AWS</title></head><body>"+
+"<img src=\"https://s3.amazonaws.com/admin-created-yzq/picture/course.jpg\"  alt=\"Error happen\" />" +
+"</body></html>"); 
+ 
+    response.end(); 
+ 
+}).listen(8080);
